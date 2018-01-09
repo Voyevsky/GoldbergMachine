82f82475571d19450e441b4738e5b436c2483944
WSEI\artur.lachor <a.lachor96@gmail.com>
2018-01-09 13:22:24 +0100

    Added camera movement and speed control scripts.

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+
+    [SerializeField] private float totalSpeed = 1f;
+    [SerializeField] private float movementSpeed = 0.1f;
+    [SerializeField] private float rotationSpeed = 0.5f;
+
+    void Update()
+    {
+
+        transform.Translate(Vector3.down * Time.deltaTime * movementSpeed * totalSpeed, Space.World);
+        transform.Translate(Vector3.right * Time.deltaTime * movementSpeed * totalSpeed * 0.5f);
+        transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed * totalSpeed, Space.World);
+    }
+    
+}
---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+
+    public float speedUpValue = 10.0f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(Time.timeScale == 1.0f)
+            {
+                Time.timeScale = speedUpValue;
+            }
+            else
+            {
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+
+}
