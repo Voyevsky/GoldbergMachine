25c610cb30952d604eb524eab9ac2a911e28c9af
Artur Lachor <a.lachor96@gmail.com>
2018-01-30 01:20:36 +0100

    Added text tip, a few stuff (including planks with restricted rotation) and a surprise at the end. :3

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -9,12 +9,29 @@ public class CameraBehavior : MonoBehaviour
     [SerializeField] private float movementSpeed = 0.1f;
     [SerializeField] private float rotationSpeed = 0.5f;
 
+    private bool move = true;
+
+    void Start()
+    {
+        Invoke("SpeedUp", 15);
+        Invoke("Stop", 37);
+    }
     void Update()
     {
-
-        transform.Translate(Vector3.down * Time.deltaTime * movementSpeed * totalSpeed, Space.World);
-        transform.Translate(Vector3.right * Time.deltaTime * movementSpeed * totalSpeed * 0.5f);
-        transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed * totalSpeed, Space.World);
+        if (move)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * movementSpeed * totalSpeed, Space.World);
+            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed * totalSpeed * 0.5f);
+            transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed * totalSpeed, Space.World);
+        }
+        
     }
-    
+    void SpeedUp()
+    {
+        totalSpeed *= 1.7f;
+    }
+    void Stop()
+    {
+        move = false;
+    }
 }
---
+++ Assets/Scripts/SurpriseRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurpriseRotation : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 10.0f;
+
+	void Update ()
+    {
+        transform.RotateAround(transform.position, transform.forward, Time.deltaTime * rotationSpeed);
+	}
+        
+}
